c33c69e6143fcb6a67d285452fc34bb16431b7aa
thao <mrtrados@gmail.com>
2023-03-27 11:02:35 +0900

    UPDATE bg colors

--- static/custom.js
+++ static/custom.js
@@ -1,22 +1,12 @@
 // Define an array of colors to choose from
 var colors = [
     "#EFE6DD" ,
-    "#F3DFA2",
-    "#D5F2E3",
     "#FDECEF",
-    "#EDC79B",
-    "#D57A66",
-    "#EABDA8",
-    "#61988E",
-    "#CEC2FF",
-    "#B3B3F1",
-    "#DCB6D5",
-    "#C7EAE4",
-    "#A7E8BD",
-    "#FCBCB8",
-    "#EFA7A7",
     "#FFEEDD",
-    "#FFD8BE"
+    "#FCEFEF",
+    "#FFFFE0",
+    "#EDF9EB",
+    "#e7f5fe"
 ];
 
 // Define a function to change the background color
